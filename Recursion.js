5be3636821d47dbc7bf7a1e4ca5306d97d1151a8
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-04-03 20:04:24 +0200

    modified the function

--- Recursion.js
+++ Recursion.js
@@ -1,14 +1,10 @@
 function isPalindrome(text) {
-if (text.length < 2) {
+    if (text.length < 2) {
         return true;
-    } else {
-        if (text.toLowerCase().split('') == text.split('').reverse().join().toLowerCase()) {
-            return true;
-        } else {
-            return false
-        }
+    } if (text[0] !== text.length[-1]) {
+        return false;
     }
+    return isPalindrome(text.substr(1, text.substr-2));
 }
 
-
-
+console.log(isPalindrome('bob'));
