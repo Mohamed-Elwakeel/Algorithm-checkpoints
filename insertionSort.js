29c0ea66dfb44a5180e33cfde64d091257d8eba4
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-04-03 20:18:51 +0200

    modified the function

--- insertionSort.js
+++ insertionSort.js
@@ -1,4 +1,11 @@
 function insertionSort(arr) {
-    return arr.sort((a, b) => a - b);
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            while (arr[j] >= arr[i]) {
+                arr[j] === arr[i];
+            }
+        }        
+    }
+    return arr[i];
 }
 
